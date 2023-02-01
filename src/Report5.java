b16b13fa2f3fe335d14acad53877779df6d060d9
Fukunaga Hikaru <e2225723@eve.u-ryukyu.ac.jp>
2023-02-01 15:00:50 +0900

    third commit

--- src/Report5.java
+++ src/Report5.java
@@ -1,12 +1,8 @@
 public class Report5 {
     public static void main(String[] args){
-        try{
-            String str = null;
-            System.out.println(str.length());
-        }
-        catch(NullPointerException e){
-            System.out.println("nullですよ" + e.getMessage());
-        } 
+        String str = "壱百万";
+        int value = Integer.parseInt(str);
+
     }
 }
 
